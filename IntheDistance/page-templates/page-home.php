024eabe1ce7c78bd18225aaba4ccb54a9ca65d09
maasaamiichii <masamichi1023@gmail.com>
2014-06-22 13:54:18 +0900

    changed design

--- IntheDistance/page-templates/page-home.php
+++ IntheDistance/page-templates/page-home.php
@@ -13,7 +13,7 @@ get_header(); ?>
 <?php endif; ?>
 
         <div class="container-inner clearfix">
-            <div id="home-content">
+            <div id="home-heading">
 <?php if ($homepage_heading = of_get_option('homepage_heading', false) ) : ?>
 		        	<h2><?php echo $homepage_heading; ?></h2>
 <?php endif; ?>
@@ -31,7 +31,22 @@ get_header(); ?>
 </div><!--container-outer-->
 
 <div class="container-outer">
-<div class="container-inner">
+    <div class="container-inner">
+        <div id="home-content">
+        <h1>Recent Updates</h1>
+        <ul class="recent-updates">
+            <?php query_posts('posts_per_page=5'); ?>
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <li>
+                <a href="<?php the_permalink(); ?>"><?php the_title();?></a>
+                <p><?php the_time( get_option( 'date_format')); ?></p>
+
+            </li>
+            <?php endwhile; endif; ?>
+            	<?php wp_reset_query(); ?>
+        </ul>
+
+
 <?php if ( have_posts() ) while ( have_posts() ){
     the_post();
     the_content();
@@ -43,7 +58,7 @@ if( $comments ) { ?>
 	<?php comments_template(); ?>
 </div>
 <?php } ?>
-
+        </div><!-- home-content-->
 </div><!-- container-inner-->
 </div><!-- container-outer-->
 <?php get_footer(); ?>
